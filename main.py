5e1f0f98ee01607e6fdd51fe4689dc4754f3d164
take2202 <fw616ytlvlv@gmail.com>
2022-06-23 14:18:25 +0900

    1st cimmit

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import time
+
+st.title('Streamlit入門')
+
+st.write('Display image')
+'Start!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i + 1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!!'
+
+
+
